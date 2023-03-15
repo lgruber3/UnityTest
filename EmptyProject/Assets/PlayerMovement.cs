c9c9c53f8588ea6464cd2bd7e06c1d8d7a2e6d34
zuranix <zuranix@gmail.com>
2023-03-15 09:41:37 +0100

    PlayerMovement Script implementation

--- EmptyProject/Assets/PlayerMovement.cs
+++ EmptyProject/Assets/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float y = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Vertical");
+        controller.Move(transform.forward * x + transform.right * y);
     }
 }
